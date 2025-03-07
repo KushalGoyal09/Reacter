c87b540940b640fa90f96be4ead0bcfc5f0b8063
Kushal Goyal <goyalkushal04@gmail.com>
2025-03-07 15:40:59 +0000

    add files to mount initailly

---
+++ src/constant/reactMountFiles.ts
@@ -0,0 +1,32 @@
+import reactInitialFiles, {Files} from './initialFiles';
+import { FileSystemTree, DirectoryNode } from '@webcontainer/api';
+
+const getMountFiles = (initialFiles: Files[]): FileSystemTree => {
+    const fileSystem: FileSystemTree = {};
+
+    for (const file of initialFiles) {
+        const pathParts = file.file_name.split('/');
+        let currentLevel = fileSystem;
+
+        for (let i = 0; i < pathParts.length - 1; i++) {
+            const part = pathParts[i];
+            if (!currentLevel[part]) {
+                currentLevel[part] = { directory: {} };
+            }
+            currentLevel = (currentLevel[part] as DirectoryNode).directory;
+        }
+
+        const fileName = pathParts[pathParts.length - 1];
+        currentLevel[fileName] = {
+            file: {
+                contents: file.content,
+            },
+        };
+    }
+
+    return fileSystem;
+};
+
+const reactMountFiles: FileSystemTree = getMountFiles(reactInitialFiles);
+
+export default reactMountFiles;
