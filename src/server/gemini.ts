1265f2c7376cd2d5a5a63e6449322f6af9a83c45
Kushal Goyal <goyalkushal04@gmail.com>
2025-07-18 06:06:10 +0000

    change model

--- src/server/gemini.ts
+++ src/server/gemini.ts
@@ -10,7 +10,7 @@ const generationConfig = {
 
 export const taltToGemini = async (prompt: string, currentFiles: Files[]) => {
     const model = genAI.getGenerativeModel({
-        model: 'gemini-2.0-flash',
+        model: 'gemini-2.5-flash',
         systemInstruction: SYSTEM_PROMPT,
     });
     const history: Content = {
